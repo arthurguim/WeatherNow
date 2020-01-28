258002f457ed156e67d6cd9d56819e492e60ee58
arthurguim <arthurguim@hotmail.com>
2020-01-28 15:27:12 -0300

    Fix cache service and Main storyboard

--- WeatherNow/WeatherNow/Service/CacheService.swift
+++ WeatherNow/WeatherNow/Service/CacheService.swift
@@ -10,11 +10,11 @@ import Foundation
 
 class CacheService {
 
-    let temporaryUrl = URL(fileURLWithPath: NSTemporaryDirectory())
+    static let temporaryUrl = URL(fileURLWithPath: NSTemporaryDirectory())
 
     func saveData(fileName: String, data: Data) {
 
-        let fileUrl = temporaryUrl.appendingPathComponent(fileName, isDirectory: false)
+        let fileUrl = CacheService.temporaryUrl.appendingPathComponent(fileName, isDirectory: false)
 
         do {
             try data.write(to: fileUrl)
@@ -25,7 +25,7 @@ class CacheService {
 
     func getAllFilesNames() -> [String]? {
         do {
-            return try FileManager.default.contentsOfDirectory(atPath: self.temporaryUrl.path)
+            return try FileManager.default.contentsOfDirectory(atPath: CacheService.self.temporaryUrl.path)
         } catch {
             print("Error getting the cached files names: \(error.localizedDescription)")
             return nil
@@ -36,7 +36,7 @@ class CacheService {
         guard let fileName = getAllFilesNames()?.first else { return nil }
 
         do {
-            let data = try Data(contentsOf: temporaryUrl.appendingPathComponent(fileName, isDirectory: false))
+            let data = try Data(contentsOf: CacheService.self.temporaryUrl.appendingPathComponent(fileName, isDirectory: false))
             return data
         } catch {
             print("Error loading data: \(error.localizedDescription)")
@@ -51,7 +51,7 @@ class CacheService {
 
         do {
             for fileName in filesNames {
-                let fileUrl = temporaryUrl.appendingPathComponent(fileName, isDirectory: false)
+                let fileUrl = CacheService.self.temporaryUrl.appendingPathComponent(fileName, isDirectory: false)
                 try fileManager.removeItem(at: fileUrl)
             }
         } catch {
